4865db693af417dfa6b28a3a1033639696446cf4
Bruno Martins <bgcngm@gmail.com>
2024-06-06 09:59:29 +0530

    veux: Use common libqti-perfd-client and power-libperfmgr

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -177,6 +177,7 @@ BOOT_SECURITY_PATCH := 2024-02-01
 VENDOR_SECURITY_PATCH := $(BOOT_SECURITY_PATCH)
 
 # SELinux
+include device/lineage/sepolicy/libperfmgr/sepolicy.mk
 include device/qcom/sepolicy_vndr/SEPolicy.mk
 SYSTEM_EXT_PRIVATE_SEPOLICY_DIRS += $(DEVICE_PATH)/sepolicy/private
 SYSTEM_EXT_PUBLIC_SEPOLICY_DIRS += $(DEVICE_PATH)/sepolicy/public
