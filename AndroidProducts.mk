88ad7f890d0cc7d2c2116974a0bed3cac9e28fd9
Subhashis2007 <itzsubhashis@proton.me>
2024-06-13 15:25:52 +0530

    veux: Adapt for Leaf

--- AndroidProducts.mk
+++ AndroidProducts.mk
@@ -5,9 +5,9 @@
 #
 
 PRODUCT_MAKEFILES := \
-    $(LOCAL_DIR)/lineage_veux.mk
+    $(LOCAL_DIR)/veux.mk
 
 COMMON_LUNCH_CHOICES := \
-    lineage_veux-user \
-    lineage_veux-userdebug \
-    lineage_veux-eng
+    veux-user \
+    veux-userdebug \
+    veux-eng
